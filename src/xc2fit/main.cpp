68ecb0e2aa2aa8415c6e28521e5529f148dc15e8
Robert Ou <rqou@robertou.com>
2016-12-28 04:26:44 -0800

    Skeleton main program

--- src/xc2fit/main.cpp
+++ src/xc2fit/main.cpp
@@ -1,7 +1,119 @@
-#include <iostream>
+/***********************************************************************************************************************
+ * Copyright (C) 2016 Robert Ou and contributors                                                                *
+ *                                                                                                                     *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General   *
+ * Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) *
+ * any later version.                                                                                                  *
+ *                                                                                                                     *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied  *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for     *
+ * more details.                                                                                                       *
+ *                                                                                                                     *
+ * You should have received a copy of the GNU Lesser General Public License along with this program; if not, you may   *
+ * find one here:                                                                                                      *
+ * https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt                                                              *
+ * or you may search the http://www.gnu.org website for the version 2.1 license, or you may write to the Free Software *
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA                                      *
+ **********************************************************************************************************************/
+
+#include "xc2fit.h"
+
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-    cout << "Hello world!\n";
+	Severity console_verbosity = Severity::NOTICE;
+
+	//Netlist file
+	string fname = "";
+
+	//Output file
+	string ofname = "";
+
+	//Parse command-line arguments
+	for(int i=1; i<argc; i++)
+	{
+		string s(argv[i]);
+
+		//Let the logger eat its args first
+		if(ParseLoggerArguments(i, argc, argv, console_verbosity))
+			continue;
+
+		else if(s == "--help")
+		{
+			ShowUsage();
+			return 0;
+		}
+		else if(s == "--version")
+		{
+			ShowVersion();
+			return 0;
+		}
+		else if(s == "-o" || s == "--output")
+		{
+			if(i+1 < argc)
+				ofname = argv[++i];
+			else
+			{
+				printf("--output requires an argument\n");
+				return 1;
+			}
+		}
+
+		//assume it's the netlist file if it'[s the first non-switch argument
+		else if( (s[0] != '-') && (fname == "") )
+			fname = s;
+
+		else
+		{
+			printf("Unrecognized command-line argument \"%s\", use --help\n", s.c_str());
+			return 1;
+		}
+	}
+
+	//Netlist filenames must be specified
+	if( (fname == "") || (ofname == "") )
+	{
+		ShowUsage();
+		return 1;
+	}
+
+	//Set up logging
+	g_log_sinks.emplace(g_log_sinks.begin(), new STDLogSink(console_verbosity));
+
+	//Print header
+	if(console_verbosity >= Severity::NOTICE)
+		ShowVersion();
+
+	LogError("\n\nHELLO WORLD\n\n");
+}
+
+void ShowUsage()
+{
+	printf(//                                                                               v 80th column
+		"Usage: xc2fit -p part -o bitstream.txt netlist.json\n"
+		"    --debug\n"
+		"        Prints lots of internal debugging information.\n"
+		"    -l, --logfile        <file>\n"
+		"        Causes verbose log messages to be written to <file>.\n"
+		"    -L, --logfile-lines  <file>\n"
+		"        Causes verbose log messages to be written to <file>, flushing after\n"
+		"        each line.\n"
+		"    -o, --output         <bitstream>\n"
+		"        Writes bitstream into the specified file.\n"
+		"    -q, --quiet\n"
+		"        Causes only warnings and errors to be written to the console.\n"
+		"        Specify twice to also silence warnings.\n"
+		"    --verbose\n"
+		"        Prints additional information about the design.\n");
+}
+
+void ShowVersion()
+{
+	printf(
+		"CoolRunner-II fitter by Robert Ou.\n"
+		"\n"
+		"License: LGPL v2.1+\n"
+		"This is free software: you are free to change and redistribute it.\n"
+		"There is NO WARRANTY, to the extent permitted by law.\n");
 }
