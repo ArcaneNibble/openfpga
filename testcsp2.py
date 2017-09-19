064d78a5114b0c61ad12af80cd44cb85b37b94cf
Robert Ou <rqou@robertou.com>
2017-09-19 02:12:25 -0700

    it works?

--- testcsp2.py
+++ testcsp2.py
@@ -48,6 +48,8 @@ def reformat_graph_nodes(e, num_nodes):
     return nodes
 
 def check_partial_assignment(dgraph_e, ngraph_e, dgraph_n, ngraph_n, assignment):
+    dgraph_e_ = set(dgraph_e)
+
     # No repeats
     count = set()
     for x in assignment:
@@ -57,36 +59,26 @@ def check_partial_assignment(dgraph_e, ngraph_e, dgraph_n, ngraph_n, assignment)
                 return False
             count.add(x)
 
-    # Sharing the node correctly
-    for (nn_in, nn_out) in ngraph_n:
-        # print("nn", nn_in, nn_out)
-        d_in = []
-        for x in nn_in:
-            if assignment[x] != -1:
-                d_in.append(dgraph_e[assignment[x]])
-        d_out = []
-        for x in nn_out:
-            if assignment[x] != -1:
-                d_out.append(dgraph_e[assignment[x]])
-        # print(d_in, d_out)
+    for srcnode_n_idx, srcport, dstnode_n_idx, dstport in ngraph_e:
+        if assignment[srcnode_n_idx] != -1 and assignment[dstnode_n_idx] != -1:
+            # Nodes are linked by an edge in the netlist
+            srcnode_d_idx = assignment[srcnode_n_idx]
+            dstnode_d_idx = assignment[dstnode_n_idx]
+            if (srcnode_d_idx, srcport, dstnode_d_idx, dstport) not in dgraph_e_:
+                return False
 
-        # Now are these sharing the same node?
-        # print("d", d_in, d_out)
-        dn = None
-        for x in d_in:
-            if dn is None:
-                dn = x[2]
-            if dn != x[2]:
-                # print("in")
-                return False
-        for x in d_out:
-            if dn is None:
-                dn = x[0]
-            if dn != x[0]:
-                # print("out")
-                return False
-    # print(assignment)
-    # print("true")
+    return True
+
+def check_one_edge(dgraph_e, ngraph_e, dgraph_n, ngraph_n, assignment, edge):
+    dgraph_e_ = set(dgraph_e)
+    srcnode_n_idx, srcport, dstnode_n_idx, dstport = edge
+    if assignment[srcnode_n_idx] != -1 and assignment[dstnode_n_idx] != -1:
+        # Nodes are linked by an edge in the netlist
+        srcnode_d_idx = assignment[srcnode_n_idx]
+        dstnode_d_idx = assignment[dstnode_n_idx]
+        if (srcnode_d_idx, srcport, dstnode_d_idx, dstport) not in dgraph_e_:
+            return False
+
     return True
 
 def ac3(dgraph_e, ngraph_e, dgraph_n, ngraph_n, domains, assignment):
@@ -95,48 +87,93 @@ def ac3(dgraph_e, ngraph_e, dgraph_n, ngraph_n, domains, assignment):
     # Set up all arcs
     q = []
 
-    def add_neighbors_to_queue(e1_):
-        e1 = ngraph_e[e1_]
-        for e2_ in ngraph_n[e1[0]][0] + ngraph_n[e1[0]][1]:
-            e2 = ngraph_e[e2_]
-            if e1 == e2:
-                continue
-            q.append((e2_, e2, e1_, e1, e1[0]))
-        for e2_ in ngraph_n[e1[2]][0] + ngraph_n[e1[2]][1]:
-            e2 = ngraph_e[e2_]
-            if e1 == e2:
-                continue
-            q.append((e2_, e2, e1_, e1, e1[2]))
+    # def add_neighbors_to_queue(e1_):
+    #     e1 = ngraph_e[e1_]
+    #     for e2_ in ngraph_n[e1[0]][0] + ngraph_n[e1[0]][1]:
+    #         e2 = ngraph_e[e2_]
+    #         if e1 == e2:
+    #             continue
+    #         q.append((e2_, e2, e1_, e1, e1[0]))
+    #     for e2_ in ngraph_n[e1[2]][0] + ngraph_n[e1[2]][1]:
+    #         e2 = ngraph_e[e2_]
+    #         if e1 == e2:
+    #             continue
+    #         q.append((e2_, e2, e1_, e1, e1[2]))
 
-    for e1_ in range(len(ngraph_e)):
-        add_neighbors_to_queue(e1_)
+    # for e1_ in range(len(ngraph_e)):
+    #     # add_neighbors_to_queue(e1_)
+
+    for e in ngraph_e:
+        q.append((e[0], e[2], e))
+        q.append((e[2], e[0], e))
 
     # print(q)
 
     while len(q):
-        tail_idx, tail_obj, head_idx, head_obj, constraint_node = q[0]
+        tail_node_idx, head_node_idx, involved_edge = q[0]
         q = q[1:]
 
         to_remove = set()
-        for x in new_domains[tail_idx]:
+        for x in new_domains[tail_node_idx]:
             any_ok = False
-            for y in new_domains[head_idx]:
+            for y in new_domains[head_node_idx]:
                 new_assignment = list(assignment)
-                new_assignment[head_idx] = y
-                any_ok = check_partial_assignment(dgraph_e, ngraph_e, dgraph_n, ngraph_n, new_assignment)
+                new_assignment[tail_node_idx] = x
+                new_assignment[head_node_idx] = y
+                any_ok = check_one_edge(dgraph_e, ngraph_e, dgraph_n, ngraph_n, new_assignment, involved_edge)
                 if any_ok:
                     break
             if not any_ok:
                 to_remove.add(x)
 
         if to_remove:
-            print("rm", tail_idx, to_remove)
+            # print("rm", tail_node_idx, to_remove)
             for x in to_remove:
-                new_domains[tail_idx].remove(x)
-            add_neighbors_to_queue(tail_idx)
+                new_domains[tail_node_idx].remove(x)
+            # Add any edges touching this node
+            tail_node = ngraph_n[tail_node_idx]
+            for e_ in tail_node[0]:
+                e = ngraph_e[e_]
+                q.append((e[0], tail_node_idx, e))
+            for e_ in tail_node[1]:
+                e = ngraph_e[e_]
+                q.append((e[2], tail_node_idx, e))
+            # print(q)
 
     # print(domains)
-    # print(new_domains)
+    print(new_domains)
+
+    return new_domains
+
+def fc(dgraph_e, ngraph_e, dgraph_n, ngraph_n, domains, assignment, selected_var):
+    new_domains = [set(x) for x in domains]
+
+    # Neighbors
+    q = []
+    tail_node = ngraph_n[selected_var]
+    for e_ in tail_node[0]:
+        e = ngraph_e[e_]
+        q.append((e[0], e))
+    for e_ in tail_node[1]:
+        e = ngraph_e[e_]
+        q.append((e[2], e))
+
+    while len(q):
+        node_idx, involved_edge = q[0]
+        q = q[1:]
+
+        to_remove = set()
+        any_ok = False
+        for y in new_domains[node_idx]:
+            new_assignment = list(assignment)
+            new_assignment[node_idx] = y
+            if not check_one_edge(dgraph_e, ngraph_e, dgraph_n, ngraph_n, new_assignment, involved_edge):
+                to_remove.add(y)
+
+        if to_remove:
+            # print("rm", tail_node_idx, to_remove)
+            for x in to_remove:
+                new_domains[node_idx].remove(x)
 
     return new_domains
 
@@ -178,10 +215,14 @@ def backtrack(dgraph_e, ngraph_e, dgraph_n, ngraph_n, domains, assignment):
             assignment[selected_var] = -1
             # print("Z")
             continue
-        new_domains = ac3(dgraph_e, ngraph_e, dgraph_n, ngraph_n, domains, assignment)
-        # new_domains = domains
-        backtrack(dgraph_e, ngraph_e, dgraph_n, ngraph_n, new_domains, assignment)
+        # new_domains = ac3(dgraph_e, ngraph_e, dgraph_n, ngraph_n, domains, assignment)
+        # new_domains = fc(dgraph_e, ngraph_e, dgraph_n, ngraph_n, domains, assignment, selected_var)
+        new_domains = domains
+        ret = backtrack(dgraph_e, ngraph_e, dgraph_n, ngraph_n, new_domains, assignment)
         assignment[selected_var] = -1
+
+        # if ret is not None:
+        #     return ret
 
 with open("testtest_graph.txt", "r") as inf:
     dgraph = read_graph(inf)
@@ -201,30 +242,17 @@ with open("testtest_graph.txt", "r") as inf:
     # print(dgraph_n)
     # print(ngraph_n)
 
-    domains = [None] * len(ngraph_e)
-    for i in range(len(ngraph_e)):
+    domains = [None] * len(ngraph_n)
+    for i in range(len(ngraph_n)):
         domains[i] = set()
-        for j in range(len(dgraph_e)):
+        for j in range(len(dgraph_n)):
             # Pre-filter the domains using the labels
-            ngraph_src_node = ngraph_e[i][0]
-            ngraph_dst_node = ngraph_e[i][2]
-            dgraph_src_node = dgraph_e[j][0]
-            dgraph_dst_node = dgraph_e[j][2]
 
-            assert len(ngraph_l[ngraph_src_node]) == 1
-            assert len(ngraph_l[ngraph_dst_node]) == 1
+            assert len(ngraph_l[i]) == 1
 
             # The node labels have to match
-            if (ngraph_l[ngraph_src_node][0] in dgraph_l[dgraph_src_node] and
-                ngraph_l[ngraph_dst_node][0] in dgraph_l[dgraph_dst_node]):
+            if (ngraph_l[i][0] in dgraph_l[j]):
+                domains[i].add(j)
 
-                # print(ngraph_src_node, ngraph_dst_node, dgraph_src_node, dgraph_dst_node)
-                # print(ngraph_l[ngraph_src_node], ngraph_l[ngraph_dst_node], dgraph_l[dgraph_src_node], dgraph_l[dgraph_dst_node])
-                # print(ngraph_e[i][1], dgraph_e[j][1], ngraph_e[i][3], dgraph_e[j][3])
-
-                # The port labels have to match as well
-                if (ngraph_e[i][1] == dgraph_e[j][1] and ngraph_e[i][3] == dgraph_e[j][3]):
-                    domains[i].add(j)
-
-    # print(domains)
-    backtrack(dgraph_e, ngraph_e, dgraph_n, ngraph_n, domains, [-1] * len(ngraph_e))
+    print(domains)
+    backtrack(dgraph_e, ngraph_e, dgraph_n, ngraph_n, domains, [-1] * len(ngraph_n))
