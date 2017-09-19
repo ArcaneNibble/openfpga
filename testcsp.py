da14654018961abb43bc22687c55a26d9bbf9b6d
Robert Ou <rqou@robertou.com>
2017-09-18 23:18:16 -0700

    work work work

--- testcsp.py
+++ testcsp.py
@@ -47,20 +47,80 @@ def reformat_graph_nodes(e, num_nodes):
 
     return nodes
 
+def check_partial_assignment(dgraph_e, ngraph_e, dgraph_n, ngraph_n, assignment):
+    # No repeats
+    count = set()
+    for x in assignment:
+        if x != -1:
+            if x in count:
+                return False
+            count.add(x)
+
+    # Sharing the node correctly
+    for (nn_in, nn_out) in ngraph_n:
+        # print(nn_in, nn_out)
+        d_in = []
+        for x in nn_in:
+            if assignment[x] != -1:
+                d_in.append(dgraph_e[x])
+        d_out = []
+        for x in nn_out:
+            if assignment[x] != -1:
+                d_out.append(dgraph_e[x])
+        # print(d_in, d_out)
+
+        # Now are these sharing the same node?
+        dn = None
+        for x in d_in:
+            if dn is None:
+                dn = x[2]
+            if dn != x[2]:
+                return False
+        for x in d_out:
+            if dn is None:
+                dn = x[0]
+            if dn != x[0]:
+                return False
+    # print(assignment)
+    return True
+
 def backtrack(dgraph_e, ngraph_e, dgraph_n, ngraph_n, domains, assignment):
+    # print("**********")
+
     if -1 not in assignment:
+        assert check_partial_assignment(dgraph_e, ngraph_e, dgraph_n, ngraph_n, assignment)
+        print("SUCCESS")
         print(assignment)
         return assignment
 
     # Variable selection choice point
-    selected_var = assignment.index(-1)
+    selected_var = -1
+    selected_var_min_remaining_vals = float('inf')
+    for i in range(len(assignment)):
+        # print("*****")
+        # print(i)
+        # print(assignment[i])
+        if assignment[i] == -1:
+            # print(selected_var_min_remaining_vals)
+            # print(len(domains[i]))
+            if len(domains[i]) < selected_var_min_remaining_vals:
+                # print("XXX")
+                selected_var_min_remaining_vals = len(domains[i])
+                selected_var = i
+
+    # print("#####")
+    # print(selected_var)
+    # print(len(domains[selected_var]))
 
     # Ordering choice point
     for choice in domains[selected_var]:
-        assignment[selected_var] = choice
-        # TODO: Check legality
-        backtrack(dgraph_e, ngraph_e, dgraph_n, ngraph_n, domains, assignment)
-        assignment[selected_var] = -1
+        new_assignment = list(assignment)
+        new_assignment[selected_var] = choice
+        print(new_assignment)
+        if not check_partial_assignment(dgraph_e, ngraph_e, dgraph_n, ngraph_n, new_assignment):
+            # print("Z")
+            continue
+        backtrack(dgraph_e, ngraph_e, dgraph_n, ngraph_n, domains, new_assignment)
 
 with open("testtest_graph.txt", "r") as inf:
     dgraph = read_graph(inf)
@@ -96,9 +156,12 @@ with open("testtest_graph.txt", "r") as inf:
             assert len(ngraph_l[ngraph_src_node]) == 1
             assert len(ngraph_l[ngraph_dst_node]) == 1
 
+            # The node labels have to match
             if (ngraph_l[ngraph_src_node][0] in dgraph_l[dgraph_src_node] and
                 ngraph_l[ngraph_dst_node][0] in dgraph_l[dgraph_dst_node]):
-                domains[i].append(j)
+                # The port labels have to match as well
+                if (ngraph_e[i][1] == dgraph_e[j][1] and ngraph_e[i][3] == dgraph_e[j][3]):
+                    domains[i].append(j)
 
-    # print(domains)
-    backtrack(dgraph_e, ngraph_e, dgraph_l, ngraph_l, dgraph_n, ngraph_n, domains, [-1] * len(ngraph_e))
+    print(domains)
+    backtrack(dgraph_e, ngraph_e, dgraph_n, ngraph_n, domains, [-1] * len(ngraph_e))
