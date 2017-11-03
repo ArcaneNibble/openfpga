7b1da654982696f14c6849b04c62fd8083c1789f
Robert Ou <rqou@robertou.com>
2017-11-03 00:22:29 -0700

    xc2par: Remove use of xbpar_rs

--- src/xc2par/src/lib.rs
+++ src/xc2par/src/lib.rs
@@ -23,9 +23,6 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-mod device;
-pub use device::{DeviceGraphNode, DeviceGraph};
-
 mod engine;
 pub use engine::*;
 
