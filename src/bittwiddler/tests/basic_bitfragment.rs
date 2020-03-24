e95b72dc4fe9353622ca9202fb00bc1587465390
R. Ou <rqou@robertou.com>
2020-03-24 00:33:23 -0700

    bittwiddler: Few more tests

--- src/bittwiddler/tests/basic_bitfragment.rs
+++ src/bittwiddler/tests/basic_bitfragment.rs
@@ -39,6 +39,40 @@ fn basic_bitfragment_encode() {
     };
     x.encode(&mut out[..], [0], [false]);
     assert_eq!(out, [true, true, false]);
+
+    // offset
+    let mut out = [true; 5];
+
+    let x = MyStruct1 {
+        field_enum: MyEnum::Choice2,
+        field_bool: false,
+    };
+    x.encode(&mut out[..], [1], [false]);
+    assert_eq!(out, [true, false, false, true, true]);
+
+    let x = MyStruct1 {
+        field_enum: MyEnum::Choice3,
+        field_bool: true,
+    };
+    x.encode(&mut out[..], [1], [false]);
+    assert_eq!(out, [true, true, true, false, true]);
+
+    // mirroring
+    let mut out = [false; 3];
+    let x = MyStruct1 {
+        field_enum: MyEnum::Choice2,
+        field_bool: false,
+    };
+    x.encode(&mut out[..], [2], [true]);
+    assert_eq!(out, [true, false, false]);
+
+    let mut out = [true; 5];
+    let x = MyStruct1 {
+        field_enum: MyEnum::Choice3,
+        field_bool: true,
+    };
+    x.encode(&mut out[..], [3], [true]);
+    assert_eq!(out, [true, false, true, true, true]);
 }
 
 #[test]
@@ -47,6 +81,43 @@ fn basic_bitfragment_decode() {
     let out = MyStruct1::decode(&x[..], [0], [false]).unwrap();
     assert_eq!(out, MyStruct1 {
         field_enum: MyEnum::Choice1,
+        field_bool: true,
+    });
+
+    let x = [false, true, true];
+    let out = MyStruct1::decode(&x[..], [0], [false]).unwrap();
+    assert_eq!(out, MyStruct1 {
+        field_enum: MyEnum::Choice4,
+        field_bool: false,
+    });
+
+    // offset
+    let x = [false, false, false, true, false, false];
+    let out = MyStruct1::decode(&x[..], [3], [false]).unwrap();
+    assert_eq!(out, MyStruct1 {
+        field_enum: MyEnum::Choice1,
+        field_bool: true,
+    });
+
+    let x = [true, true, true, false, true, true];
+    let out = MyStruct1::decode(&x[..], [3], [false]).unwrap();
+    assert_eq!(out, MyStruct1 {
+        field_enum: MyEnum::Choice4,
+        field_bool: false,
+    });
+
+    // mirroring
+    let x = [true, false, false];
+    let out = MyStruct1::decode(&x[..], [2], [true]).unwrap();
+    assert_eq!(out, MyStruct1 {
+        field_enum: MyEnum::Choice2,
+        field_bool: false,
+    });
+
+    let x = [true, true, true, false, true, true];
+    let out = MyStruct1::decode(&x[..], [5], [true]).unwrap();
+    assert_eq!(out, MyStruct1 {
+        field_enum: MyEnum::Choice3,
         field_bool: true,
     });
 }
